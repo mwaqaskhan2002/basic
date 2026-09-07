7bb1584f940aef92a1bf839650ccec6dcc161d73
Waqas Khan <mwaqaskhan2002>
2026-09-07 11:28:18 +0500

    Learned about numbers and math in js

---
+++ numbers_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1)); // come in handy in ecommerce applications
+
+const otherNumber = 12323.896
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-PK'));
+
+// ++++++++++++++ Maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(5, 8, 4, 15, 54, 1));
+// console.log(Math.max(5, 8, 4, 15, 54, 1));
+// console.log(Math.random(5, 8, 4, 15, 54, 1));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
