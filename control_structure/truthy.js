55edbaac2387fd475d01811f8a8668ae3fec5cb6
Waqas Khan <mwaqaskhan2002>
2026-09-11 17:31:18 +0500

    Learned about if else and truthy

--- control_structure/truthy.js
+++ control_structure/truthy.js
@@ -1,10 +1,10 @@
-const userEmail = ""
+// const userEmail = ""
 
-if (userEmail){
-    console.log("Got User Email");
-} else {
-    console.log("Don't have user email");
-}
+// if (userEmail){
+//     console.log("Got User Email");
+// } else {
+//     console.log("Don't have user email");
+// }
 
 // false values 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
@@ -12,12 +12,28 @@ if (userEmail){
 // truthy values
 //  "0", 'false', " ", [], {}, function(){}
 
-if (userEmail.length === 0){
-    console.log("Array is empty");
-}
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+// Nullsih Coalescing operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 10
+
+console.log(val1);
+
+// Ternary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("More than 80");
